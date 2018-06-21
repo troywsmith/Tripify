a1e06d4a8de58e2279b63bf0b8944a33c1757649
Jae1xp <jaeson722@gmail.com>
2018-06-21 17:38:24 -0400

    made list component and binded to click in dashboard

---
+++ src/List/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class List extends Component {
+  render() {
+    return (
+
+      <div className="list">
+        LIST
+      </div>
+
+    );
+  }
+}
+
+export default List;
