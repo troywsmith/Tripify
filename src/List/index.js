0deb6654207dbaf91e1b1cbf2091557eb1a03397
Jae1xp <jaeson722@gmail.com>
2018-06-21 18:51:29 -0400

    made list-item form. No functionality yet

--- src/List/index.js
+++ src/List/index.js
@@ -4,16 +4,30 @@ class List extends Component {
   render() {
     return (
       <div className="list">
-        <div className="list-div">
-        <h3>List</h3>
-        <ul className="list-list"> 
-        {this.props.api.list.map(item => 
-        <li key={item.list_id}>{item.list_name}</li>
-        )}
-        </ul>
-        </div>
-      </div>
+        <h1>Create a new List Item!</h1>
+          <div className="list-div">
+          <h3>List</h3>
+          <ul className="list-list"> 
+          {this.props.api.list.map(item => 
+          <li key={item.list_id}>{item.list_name}</li>
+          )}
+          </ul>
+          </div>
 
+        <form>
+          <p>
+            <label for="list-item">List Item</label>
+            <input
+              type="text"
+              name="list_name"
+              // value={this.state.name}
+            />
+          </p>
+          <p>
+            <input type="submit" value="Submit" />
+          </p>
+        </form>
+      </div>
     );
   }
 }
