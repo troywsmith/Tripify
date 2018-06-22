5a351f4feb286e6da1e0aef516c49aba49cf434b
Troy <36167978+troywsmith@users.noreply.github.com>
2018-06-22 10:22:07 -0400

    connect db for user and itinerary components

--- database/schema.sql
+++ database/schema.sql
@@ -14,29 +14,32 @@ DROP TABLE IF EXISTS list;
 
 CREATE TABLE users (
   user_id SERIAL PRIMARY KEY,
-  user_name TEXT NOT NULL
+  user_name TEXT NOT NULL,
+  user_img TEXT
 );
 
 CREATE TABLE trips (
   trip_id SERIAL PRIMARY KEY,
   trip_name TEXT NOT NULL,
-  trip_password_digest TEXT NOT NULL
+  trip_password_digest TEXT NOT NULL,
+  trip_description TEXT
 );
 
 CREATE TABLE activities (
   activity_id SERIAL PRIMARY KEY,
   activity_name TEXT NOT NULL,
-  user_id INTEGER REFERENCES users(user_id),
+  date TEXT NOT NULL,
+  time TEXT NOT NULL,
   trip_id INTEGER REFERENCES trips(trip_id)
+);
+
+CREATE TABLE list (
+  list_id SERIAL PRIMARY KEY,
+  item TEXT NOT NULL
 );
 
 CREATE TABLE user_trip (
   user_trip_id SERIAL PRIMARY KEY,
   user_id INTEGER REFERENCES users(user_id),
   trip_id INTEGER REFERENCES trips(trip_id)
-);
-
-CREATE TABLE list (
-  list_id SERIAL PRIMARY KEY,
-  list_name TEXT NOT NULL
 );
