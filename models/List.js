6855c887f914c9da72ad1f88a06527be4327e366
Troy <36167978+troywsmith@users.noreply.github.com>
2018-06-25 12:54:00 -0400

    update

--- models/List.js
+++ models/List.js
@@ -6,8 +6,21 @@ List.all = () => {
   return db.any('SELECT * FROM list');
 };
 
+List.find = id => {
+  return db.one('SELECT * FROM list WHERE list_id = $<id>', { id: id });
+}
+
 List.create = newListItem => {
-  return db.one('INSERT INTO list (item) VALUES ($<name>) RETURNING *', newListItem);
+  return db.one('INSERT INTO list (item) VALUES ($<item>) RETURNING *', newListItem);
 };
+
+List.update = updateListItem => {
+  return db.none(`UPDATE list SET 
+  item = $<item> WHERE list_id = $<id>`, updateListItem);
+}
+
+List.delete = id => {
+  return db.result(`DELETE FROM list WHERE list_id = $<id>`, {id: id})
+}
 
 module.exports = List;
